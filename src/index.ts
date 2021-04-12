bdf172a5a9bfd4dc78b2bb212a651de0f38eb895
Dmitry Shirokov <dshirokov@seek.com.au>
2021-04-13 08:23:49 +1000

    debugging

--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,65 @@
 // const path = require('path');
 const fs = require("fs");
 
+// https://www.nasdaqtrader.com/content/technicalsupport/specifications/dataproducts/NQTVITCHSpecification.pdf
 const source = "/Users/dshirokov/Downloads/01302020.NASDAQ_ITCH50";
 
-const readInt = (offset: number, length: number, buf: Buffer) => {
-	var value = 0;
-	for (var i = length - 1; i >= 0; i--) {
-		value = (value * 256) + buf[offset + i];
-	}
+// const readInt = (offset: number, length: number, buf: Buffer) => {
+// 	let val = BigInt(0);
+// 	const eight = BigInt(8);
+// 	for (let i = 0; i < length; i++) {
+// 		val = (val << eight) + BigInt(buf[offset + i]);
+// 	}
+// 	return val
+// }
 
-	return value;
-
-	// let val = 0;
-	// for (let i = 0; i < length; i++) {
-	// 	val = (val << 8) + buf[offset + i];
-	// }
-	// return val
-}
-
-
-// Order book: (type A,P,D,R,E,C)
+// Order book: (type A, P, D, R, E, C)
 const parse = (type: string, buf: Buffer) => {
 	if (type == 'A') {
-		/*
-
-Order Reference Number
-11 8 Integer The unique reference number assigned to the neworder at
-the time of receipt.
-Buy/Sell Indicator 19 1 Alpha The type of order being added.
-“B” =Buy Order. “S” = SellOrder.
-Shares 20 4 Integer The total number of shares associated with the order being
-added to the book.
-Stock 24 8 Alpha Stock symbol, right padded with spaces
-Price 32 4 Price (4) */
 		return {
 			type,
 			locate: buf.readUInt16BE(1),
 			tracking: buf.readUInt16BE(3),
-			timestamp: readInt(5, 6, buf),
+			timestamp: parseInt(buf.toString('hex', 5, 11), 16),
 			ref: buf.toString('hex', 11, 19),
-			side: buf.toString('hex', 19, 20),
-			shares: buf.toString('hex', 20, 24),
+			side: buf.toString('latin1', 19, 20),
+			shares: buf.readUInt32BE(20),
 			stock: buf.toString('latin1', 24, 32),
 			price: buf.readUInt32BE(32),
 		}
 	}
-	// if (type === "S") {
-	// 	return {
-	// 		type,
-	// 		locate: buf.readUInt16BE(1),
-	// 		tracking: buf.readUInt16BE(3),
-	// 		timestamp: readInt(5, 6, buf),
-	// 		eventCode: buf.toString('latin1', 11, 12)
-	// 	};
-	// }
+
+	if (type === 'P') {
+		return {
+			type,
+			locate: buf.readUInt16BE(1),
+			tracking: buf.readUInt16BE(3),
+			timestamp: parseInt(buf.toString('hex', 5, 11), 16),
+			ref: buf.toString('hex', 11, 19),
+			side: 'B',
+			shares: buf.readUInt32BE(20),
+			stock: buf.toString('latin1', 24, 32),
+			price: buf.readUInt32BE(32),
+		}
+	}
+
+	if (type === "S") {
+		return {
+			type,
+			locate: buf.readUInt16BE(1),
+			tracking: buf.readUInt16BE(3),
+			timestamp: parseInt(buf.toString('hex', 5, 11), 16),
+			eventCode: buf.toString('latin1', 11, 12)
+		};
+	}
+
 	if (type === "R") {
 		return {
 			type,
 			locate: buf.readUInt16BE(1),
 			tracking: buf.readUInt16BE(3),
-			timestamp: readInt(5, 6, buf),
+			timestamp: parseInt(buf.toString('hex', 5, 11), 16),
 			stock: buf.toString('latin1', 11, 19),
-			// Note: there's more fields
 		}
 	}
 
@@ -70,36 +68,50 @@ Price 32 4 Price (4) */
 // const b = Buffer.from('0a4142494f20202020534e000000644e435a20504e4e324e000000004e002752001e0000', 'hex')
 // console.log(b)
 
-const stream = fs.createReadStream(source, { start: 0, end: 4 * 100000 });
-let leftover: Buffer;
+// const stream = fs.createReadStream(source, { start: 0, end: 10 * 7000000 });
+const stream = fs.createReadStream(source);
+let leftover: Buffer | undefined;
 let seq = 0;
 stream.on("data", (chunk: Buffer) => {
 	const buf = leftover !== undefined ? Buffer.concat([leftover, chunk]) : chunk;
 
-	console.log('> chunk', chunk.length)
-	console.log('> leftover', leftover?.length ?? 0)
+	// console.log('> chunk', chunk.length)
+	// console.log('> leftover', leftover?.length ?? 0)
 	leftover = undefined;
 
 	let offset = 0;
 	let size = 0;
 	do {
+		if (offset + 2 > buf.length) {
+			leftover = buf.slice(offset)
+			break;
+		}
+
 		size = buf.readUInt16BE(offset)
 		if (offset + 2 + size > buf.length) {
 			leftover = buf.slice(offset)
 			break;
 		}
+		seq++;
 
 		const type = buf.toString('latin1', offset + 2, offset + 3)
 		const message = buf.slice(offset + 2, offset + 3 + size - 1)
-		// if (type === 'R') {
-		console.log(seq++, type, message)
-		// 	console.log(parse(type, message))
-		// }
+
+		if (type === 'A') {
+			const msg = parse(type, message)
+			// console.log(msg)
+		}
+		if (seq % 1e6 == 0) console.log('> seq', seq)
 
 		offset = offset + 2 + size;
 	} while (offset < buf.length);
+
+	if (seq > 20 * 1e6) {
+		console.log("Hard stop")
+		process.kill(1)
+	}
 });
 
-stream.on("end", (chunk) => {
-	console.log("> end", chunk);
+stream.on("end", () => {
+	console.log("> end");
 });
