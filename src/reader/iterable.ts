f84e9adc12c12fccf21c795a91021f98c1d7df18
Dmitry Shirokov <dshirokov@seek.com.au>
2021-04-20 08:59:37 +1000

    little changes

--- src/reader/iterable.ts
+++ src/reader/iterable.ts
@@ -8,36 +8,36 @@ const PAGE_SIZE = 65536;
 export default (fd: number) => {
   let offset = 0;
   let size = 0;
-  let chunk: Buffer;
-  let chunkOffset: number;
+  let page: Buffer;
+  let pageOffset: number;
 
   const stats = fs.fstatSync(fd);
   const totalSize = stats.size;
 
-  const readChunk = () => {
-    chunkOffset = 0;
-    chunk = Buffer.alloc(PAGE_SIZE);
-    fs.readSync(fd, chunk, 0, PAGE_SIZE, offset);
+  const readPage = () => {
+    pageOffset = 0;
+    page = Buffer.alloc(PAGE_SIZE);
+    fs.readSync(fd, page, 0, PAGE_SIZE, offset);
   }
 
   const iterator: IterableIterator<Buffer> = {
     [Symbol.iterator]: function () { return this; },
     next: () => {
-      if (!chunk) readChunk();
+      if (!page) readPage();
 
-      if (chunkOffset + 2 > chunk.length) {
-        readChunk();
+      if (pageOffset + 2 > page.length) {
+        readPage();
       }
 
-      size = chunk.readUInt16BE(chunkOffset);
-      if (chunkOffset + 2 + size > chunk.length) {
-        readChunk()
+      size = page.readUInt16BE(pageOffset);
+      if (pageOffset + 2 + size > page.length) {
+        readPage()
       }
 
-      // TODO: do not copy
-      const value = chunk.slice(chunkOffset + 2, chunkOffset + 2 + size);
+      // TODO: do not copy?
+      const value = page.slice(pageOffset + 2, pageOffset + 2 + size);
       offset = offset + 2 + size;
-      chunkOffset = chunkOffset + 2 + size;
+      pageOffset = pageOffset + 2 + size;
 
       return { value, done: offset === totalSize }
     }
